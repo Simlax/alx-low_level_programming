54b29f83086e3dd72ad54193409e7fdc0e0fed3d
Sim Lax <tomandjerryweb@gmail.com>
2022-06-12 14:16:38 -0700

    show size of diferent type

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,13 @@
+#include <stdio.h>
+
+/**                                                                                                                                                                                             
+ * main - Entry point                                                                                                                                                                           
+ *                                                                                                                                                                                              
+ * Return: Always 0 (Success)                                                                                                                                                                   
+ *                                                                                                                                                                                              
+ */  
 int main(void)
-	/**
-	 *  * main - Entry point
-	 *   *
-	 *    * Return: Always 0 (Success)
-	 *     */
+
 	
 {
 char acharacter;
